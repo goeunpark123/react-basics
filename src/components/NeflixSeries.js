5ab167c8704a8ab73c999ea5779655bd87fd5b86
goeunpark123 <goeunpark21@gmail.com>
2023-06-15 23:46:23 +0900

    feat list page

---
+++ src/components/NeflixSeries.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./NeflixSeries.css";
+
+const NetflixSeries = () => {
+  return (
+    <ul className="list">
+      <li>Never Have I ever</li>
+      <li>Ginny & Georgia</li>
+      <li>Beef</li>
+    </ul>
+  );
+};
+
+export default NetflixSeries;
