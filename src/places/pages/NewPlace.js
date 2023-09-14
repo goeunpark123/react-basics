029c4bb7336834ea6503480181315dfa6555ca9d
goeunpark123 <goeunpark21@gmail.com>
2023-09-14 14:54:50 +0900

    add form hook

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,67 @@
 import React from "react";
 import "./NewPlace.css";
+import "./PlaceForm.css";
 
+import { useForm } from "../../shared/hooks/form-hook";
 import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validator";
 
 const NewPlace = () => {
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
+
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Invalid input."
+        onInput={inputHandler}
+      />
+
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Invalid input(at least 5 characters)."
+        onInput={inputHandler}
+      />
+
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Invalid input."
+        onInput={inputHandler}
+      />
+
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,105 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./PlaceForm.css";
+
+import { useForm } from "../../shared/hooks/form-hook";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validator";
+
+const PLACES = [
+  {
+    id: "p1",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/8/80/Kensington_Palace%2C_the_South_Front_-_geograph.org.uk_-_287402.jpg",
+    title: "Kensington Palace",
+    description:
+      "The palace and gardens feature exhibitions related to people who lived here.",
+    address: "Kensington Gardens, London W8 4PX",
+    creator: "u2",
+  },
+  {
+    id: "p2",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/4/4d/White_House%2C_Blue_Sky.jpg",
+    title: "The White House",
+    description: "Home & office of the United States president.",
+    address: "1600 Pennsylvania Avenue NW, Washington, DC 20500",
+    creator: "u2",
+  },
+  {
+    id: "p3",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    title: "Empire State Building",
+    description:
+      "Iconic, art deco office tower from 1931 with exhibits & observatories on the 86th & 102nd floors.",
+    address: "20 W 34th St., New York, NY 10001",
+    creator: "u1",
+  },
+];
+
+const UpdatePlace = () => {
+  const placeId = useParams().placeId;
+  const identifiedPlace = PLACES.find((p) => p.id === placeId);
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedPlace.title,
+        isValid: true,
+      },
+      description: {
+        value: identifiedPlace.description,
+        isValid: true,
+      },
+    },
+    true
+  );
+
+  const placeUpdateSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
+  if (!identifiedPlace) {
+    return (
+      <div className="center">
+        <h2>Could not find place.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+      <Input
+        id="title"
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Invalid input."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Invalid input."
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        UPDATE PLACE
+      </Button>
+    </form>
+  );
+};
+
+export default UpdatePlace;
